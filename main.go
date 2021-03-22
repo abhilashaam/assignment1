9661c4bb0c6dd699438625304d6b40793e66370c
abhilashaam <abhilashaamgowda@gmail.com>
2021-03-22 14:42:15 +0530

    update to main

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("remote commit")
+	fmt.Printf("commit1")
+	fmt.Printf("commit2")
+	fmt.Printf("commit21")
+	fmt.Printf("trying to commit")
+}
