4fafcecb8cc73b3822c3e9975acea207e81a12c0
abhilashaam <abhilashaamgowda@gmail.com>
2021-03-22 13:02:55 +0530

    first commit
    commit2
    
    trying to commit

--- main.go
+++ main.go
@@ -2,5 +2,10 @@ package main
 
 import "fmt"
 
-func main(){
-fmt.Printf("remote commit")}
+func main() {
+	fmt.Printf("remote commit")
+	fmt.Printf("commit1")
+	fmt.Printf("commit2")
+	fmt.Printf("commit3")
+	fmt.Printf("trying to commit")
+}
